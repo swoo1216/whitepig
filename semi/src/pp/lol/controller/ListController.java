bd2d786c8a5d678c9e5d82e3d3102b237b6f28b7
SW <SW@Mozzi-1216>
2018-08-21 16:39:16 +0900

    인덱스바꿈

---
+++ semi/src/pp/lol/controller/ListController.java
@@ -0,0 +1,28 @@
+package pp.lol.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/list.do")
+public class ListController extends HttpServlet
+{
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String spageNum=request.getParameter("pageNum");
+		int pageNum=1;
+		if(spageNum!=null)
+		{
+			pageNum=Integer.parseInt(spageNum);
+		}
+		int startRow=(pageNum-1)*5+1;
+		int endRow=startRow+4;
+		LoLBoardDao dao=new LoLBoardDao();
+		ArrayList<LoLBoardVo> list=dao.list(startRow,endRow);
+	}
+}
