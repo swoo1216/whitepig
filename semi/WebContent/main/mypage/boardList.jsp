fed1c3ca4850079a9482d02f9821e9d1ad1818d0
SW <SW@Mozzi-1216>
2018-08-31 14:20:54 +0900

    23233

--- semi/WebContent/main/mypage/boardList.jsp
+++ semi/WebContent/main/mypage/boardList.jsp
@@ -9,7 +9,8 @@
 </head>
 <body>
 <h2>게시글목록</h2>
-<table border="1" width="500">
+<table border="2" width="500" style="text-align: center;">
+	<tr>go</tr>
 	<tr>
 		<th>글번호</th>
 		<th>제목</th>
@@ -18,7 +19,70 @@
 		<th>조회수</th>
 		<th>작성일</th>
 	</tr>
-	<c:forEach var="vo" items="${list }">
+	<c:forEach var="vo" items="${list1 }">
+	<tr>
+		<td>${vo.bnum}</td>
+		<td>${vo.title}</td>
+		<td>${vo.id }</td>
+		<td>${vo.hit }</td>
+		<td>${vo.recomm}</td>
+		<td>${vo.regdate }</td>
+	</tr>
+	</c:forEach>
+</table>
+<table border="2" width="500" style="text-align: center;">
+	<tr>poke</tr>
+	<tr>
+		<th>글번호</th>
+		<th>제목</th>
+		<th>작성자</th>
+		<th>추천수</th>
+		<th>조회수</th>
+		<th>작성일</th>
+	</tr>
+	<c:forEach var="vo" items="${list2 }">
+	<tr>
+		<td>${vo.bnum}</td>
+		<td>${vo.title}</td>
+		<td>${vo.id }</td>
+		<td>${vo.hit }</td>
+		<td>${vo.recomm}</td>
+		<td>${vo.regdate }</td>
+	</tr>
+	</c:forEach>
+</table>
+<table border="2" width="500" style="text-align: center;">
+	<tr>lol</tr>
+	<tr>
+		<th>글번호</th>
+		<th>제목</th>
+		<th>작성자</th>
+		<th>추천수</th>
+		<th>조회수</th>
+		<th>작성일</th>
+	</tr>
+	<c:forEach var="vo" items="${list3 }">
+	<tr>
+		<td>${vo.bnum}</td>
+		<td>${vo.title}</td>
+		<td>${vo.id }</td>
+		<td>${vo.hit }</td>
+		<td>${vo.recomm}</td>
+		<td>${vo.regdate }</td>
+	</tr>
+	</c:forEach>
+</table>
+<table border="2" width="500" style="text-align: center;">
+	<tr>music</tr>
+	<tr>
+		<th>글번호</th>
+		<th>제목</th>
+		<th>작성자</th>
+		<th>추천수</th>
+		<th>조회수</th>
+		<th>작성일</th>
+	</tr>
+	<c:forEach var="vo" items="${list4 }">
 	<tr>
 		<td>${vo.bnum}</td>
 		<td>${vo.title}</td>
--- semi/src/pp/mypage/controller/CommentListController.java
+++ semi/src/pp/mypage/controller/CommentListController.java
@@ -19,6 +19,7 @@ public class CommentListController extends HttpServlet
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		String id = (String)request.getSession().getAttribute("id");
 		String spageNum=request.getParameter("pagenum");
 		int pageNum=1;
 		if(spageNum!=null)
@@ -28,17 +29,40 @@ public class CommentListController extends HttpServlet
 		int startRow=(pageNum-1)* 5+1;
 		int endRow=startRow +4;
 		MypageDao dao = new MypageDao();
-		ArrayList<MypageVo> commentlist=dao.commentlist(startRow, endRow);
-		int pageCount=(int)Math.ceil(dao.getCount()/10.0);
+		ArrayList<MypageVo> commentlist1=dao.commentlist1(startRow, endRow,id);
+		ArrayList<MypageVo> commentlist2=dao.commentlist2(startRow, endRow,id);
+		ArrayList<MypageVo> commentlist3=dao.commentlist3(startRow, endRow,id);
+		ArrayList<MypageVo> commentlist4=dao.commentlist4(startRow, endRow,id);
+		int pageCount1=(int)Math.ceil(dao.getCount1()/10.0);
+		int pageCount2=(int)Math.ceil(dao.getCount2()/10.0);
+		int pageCount3=(int)Math.ceil(dao.getCount3()/10.0);
+		int pageCount4=(int)Math.ceil(dao.getCount4()/10.0);
 		int startPage=((pageNum-1/4*4)+1);
 		int endPage=startPage+3;
-		if(endPage>pageCount)
+		if(endPage>pageCount1)
 		{
-			endPage=pageCount;
+			endPage=pageCount1;
 		}
-		request.setAttribute("commentlist", commentlist);
-		System.out.println(commentlist);
-		request.setAttribute("pageCount", pageCount);
+		else if(endPage>pageCount2)
+		{
+			endPage=pageCount2;
+		}
+		else if(endPage>pageCount3)
+		{
+			endPage=pageCount3;
+		}
+		else if(endPage>pageCount4)
+		{
+			endPage=pageCount4;
+		}
+		request.setAttribute("commentlist1", commentlist1);
+		request.setAttribute("commentlist2", commentlist2);
+		request.setAttribute("commentlist3", commentlist3);
+		request.setAttribute("commentlist4", commentlist4);
+		request.setAttribute("pageCount1", pageCount1);
+		request.setAttribute("pageCount2", pageCount2);
+		request.setAttribute("pageCount3", pageCount3);
+		request.setAttribute("pageCount4", pageCount4);
 		request.setAttribute("startPage", startPage);
 		request.setAttribute("endPage", endPage);
 		request.setAttribute("pageNum", pageNum);
