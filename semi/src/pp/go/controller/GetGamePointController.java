d470f3bde3ca0dc6e0081e442dc108e7a35921c4
a <a@DESKTOP-O3EQHO5>
2018-08-31 17:44:20 +0900

    bcvbnm,.

--- semi/src/pp/go/controller/GetGamePointController.java
+++ semi/src/pp/go/controller/GetGamePointController.java
@@ -1,6 +1,8 @@
 package pp.go.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import pp.go.dao.GameScoreDao;
 import pp.go.dao.GuserDao;
+import pp.go.vo.GameScoreVo;
 
 @WebServlet("/go/getGamePoint.do")
 public class GetGamePointController extends HttpServlet {
@@ -25,7 +32,38 @@ public class GetGamePointController extends HttpServlet {
 		GuserDao.getInstance().upPoint(id, getPoint);
 
 		int point = GuserDao.getInstance().select(id).getPoint();
+		GameScoreVo vo = new GameScoreVo(0, id, getPoint);
+		GameScoreDao.getInstance().insert(vo);
+		//request.getSession().setAttribute("point", point);
 
-		request.getSession().setAttribute("point", point);
+		ArrayList<GameScoreVo> list = GameScoreDao.getInstance().list();
+
+		JSONArray arr = new JSONArray();
+		for (GameScoreVo voo : list) {
+			int vgameNum = voo.getGameNum();
+			String vid = voo.getId();
+			int vgetPoint = voo.getGetPoint();
+
+			JSONObject gnum = new JSONObject();
+			JSONObject gid = new JSONObject();
+			JSONObject gpoint = new JSONObject();
+
+			gnum.put("gameNum", vgameNum);
+			gid.put("id", gid);
+			gpoint.put("getPoint", vgetPoint);
+
+			JSONArray users = new JSONArray();
+
+			users.add(gnum);
+			users.add(gid);
+			users.add(gpoint);
+
+			arr.add(users);
+		}
+
+		response.setContentType("text/plain; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.println(arr.toString());
+		pw.close();
 	}
 }
